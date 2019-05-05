fc31890f884b1350df21d85b463cc8906f5e0053
Marco Frisina <marco@frisina.xyz>
2019-05-05 13:19:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+var emptyArray = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < musicians.length; i++) {
+    var statement = musicians[i] + ' plays ' + instruments[i]
+	emptyArray.push(statement);
+  }
+  return emptyArray;
+}
+
+
+
+function johnLennonFacts(facts) {
+  var secondEmptyArray = [];
+  var counter = 0;
+  while ( counter < facts.length) {
+    secondEmptyArray.push(facts[counter] + "!!!");
+    counter++;
+  }
+  console.log(secondEmptyArray);
+  return secondEmptyArray;
+}
+
+
+function iLoveTheBeatles(number) {
+  var emptyArray = [];
+  number = number + 1;
+
+
+}
